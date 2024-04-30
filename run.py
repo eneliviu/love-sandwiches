e3435ac7a0ad424695059cb1ffb14bd7c9371227
Liviu Ene <eneliviu@gmail.com>
2024-04-30 14:35:05 +0200

    Create get_sales_data func to get data string from user

--- run.py
+++ run.py
@@ -2,18 +2,29 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive"
+#     ]
 
-sales = SHEET.worksheet('sales')
+# CREDS = Credentials.from_service_account_file('creds.json')
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-data = sales.get_all_values()
-print(data)
+
+def get_sales_data():
+    '''
+    Get sales figures input fro the user
+    '''
+    print('Please enter sales data from the last market.')
+    print('Data should be six numbers, separated by comas.')
+    print('Example: 10, 20, 30, 40, 50, 60\n')
+
+    data_str = input('Enter your data here: ')
+    print(f'The data provided is {data_str}')
+
+
+get_sales_data()
