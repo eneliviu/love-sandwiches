7e36824e5fb598e43f91e79dc7ee07552f5a5f6f
Liviu Ene <eneliviu@gmail.com>
2024-04-30 15:08:07 +0200

    Add sales data to worksheet

--- run.py
+++ run.py
@@ -2,20 +2,23 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# SCOPE = [
-#     "https://www.googleapis.com/auth/spreadsheets",
-#     "https://www.googleapis.com/auth/drive.file",
-#     "https://www.googleapis.com/auth/drive"
-#     ]
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-# CREDS = Credentials.from_service_account_file('creds.json')
-# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-# SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     '''
-    Get sales figures input fro the user
+    Get sales figures input fro the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated 
+    by commas.The loop will repeatedly request data, until it is valid. 
     '''
 
     while True:
@@ -53,4 +56,16 @@ def validate_data(values):
     return True
 
 
+def update_sales_worksheet(data):
+    '''
+    Update sales worksheet, add new row with the list data provided.
+    '''
+    print('Updating sales worksheet...\n')
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('Sales worksheet updated succesfully.\n')
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
